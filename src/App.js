ce4386e3f80c46cf9f687b13cb632e0736863d34
EDUARDO AUGUSTO GALVÃO <18262521@CFP127ALUNO.LOCAL>
2020-02-06 11:17:12 -0300

    Adicionando função para adicionar um novo dentista pelo formulário

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Tabela from './dentista/TabelaDentista'
 import Formulario from './dentista/FormularioDentista'
@@ -11,6 +10,7 @@ class App extends Component {
     dentistas: []
   };
 
+  //Método para carregar todos os dentistas assim que a aplicação for montada
   componentDidMount(){
     axios.get('http://localhost:8080/odonto/dentistas')
       .then((res) => {
@@ -19,10 +19,25 @@ class App extends Component {
       })
   }
 
+  enviarDados = (dentista) =>{
+
+    axios.post('http://localhost:8080/odonto/dentistas', dentista)
+    .then((res) => {
+
+      const newState = [...this.state.dentistas, res.data]
+
+      this.setState({
+        dentistas: newState
+      })
+      
+    });
+
+  }
+
   render(){
     return (
       <div>
-        <Formulario />
+        <Formulario enviarDados={this.enviarDados}/>
         <Tabela listaDentistas={this.state.dentistas} />
       </div>
     );
--- src/dentista/FormularioDentista.js
+++ src/dentista/FormularioDentista.js
@@ -15,22 +15,60 @@ class FormularioDentista extends Component{
 
         this.state = this.stateInicial;
     }
+
+    //Listener de mudança de valor do input
+    inputHandler = (event) => {
+
+        const {name, value} = event.target
+
+        this.setState({
+
+            //Utilizando [] para pegar a propriedade do json
+            //name será o name do objeto que setou o evento
+            //value será o valor do objeto que setou o evento
+            [name]: value
+        });
+
+    }
+
+    submitHandler = (event) =>{
+
+        event.preventDefault();
+
+        this.props.enviarDados(this.state);
+
+        this.setState({
+            codigo: '',
+            nome: '',
+            cro: '',
+            email: '',
+            telefone: ''
+        })
+
+    }
+
     render(){
+        //Criando variáveis por interpolação
+        const {codigo, nome, cro, telefone, email} = this.state;
+        
         return(
             <form>
+                <label htmlFor="codigo">Código:</label>
+                <input type="text" id="codigo" name="codigo" value={codigo} onChange={this.inputHandler}/>
+
                 <label htmlFor="nome">Nome:</label>
-                <input type="text" id="nome" name="nome" />
+                <input type="text" id="nome" name="nome" value={nome} onChange={this.inputHandler}/>
 
                 <label htmlFor="cro">CRO:</label>
-                <input type="text" id="cro" name="cro" />
+                <input type="text" id="cro" name="cro" value={cro} onChange={this.inputHandler}/>
 
                 <label htmlFor="telefone">Telefone:</label>
-                <input type="text" id="telefone" name="telefone" />
+                <input type="text" id="telefone" name="telefone" value={telefone} onChange={this.inputHandler}/>
 
                 <label htmlFor="email">Email:</label>
-                <input type="text" id="email" name="email" />
+                <input type="text" id="email" name="email" value={email} onChange={this.inputHandler}/>
 
-                <button type="submit">Gravar</button>
+                <button type="submit" onClick={this.submitHandler}>Gravar</button>
 
             </form>
         );
